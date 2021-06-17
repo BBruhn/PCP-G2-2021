d9c376dfe859a6af13302eee634adbba98896e38
BBruhn <bruhnbenjamin64@gmail.com>
2021-06-17 12:56:51 -0400

    Update Detector de caras.py
    Este código cumple la función de detectar caras en una imagen y escribir en esta el número de caras que encuentra.

--- Detector de caras.py
+++ Detector de caras.py
@@ -1,37 +1,36 @@
 import cv2
 import dlib
-from skimage import color
 from skimage import io
 
 
-detector = dlib.get_frontal_face_detector()
+detector = dlib.get_frontal_face_detector() ##función que detecta caras##
 
-img = io.imread('image.jpg')
+img = io.imread('image.jpg')##bajar la imágen##
 
-faces = detector(img)
-i = 0
+faces = detector(img) ##aplicar la función a la imágen##
+i = 0 ##contador de caras##
 for face in faces:
-    x1 = face.left()
+    x1 = face.left() ##coordenadas de referencia para dibujar el rectángulo##
     y1 = face.top()
     x2 = face.right()
     y2 = face.bottom()
 
 
-    i = i+1
-    cv2.rectangle(img=img, pt1=(x1, y1), pt2=(x2, y2), color=(0, 255, 0), thickness=4)
-texto = "Hay "+ str(i) + " caras"
+    i = i+1 ##aumento del contador por iteración de caras##
+    cv2.rectangle(img=img, pt1=(x1, y1), pt2=(x2, y2), color=(0, 255, 0), thickness=4) ##dibuja un rectangulo alrededor de la cara##
+texto = "Hay "+ str(i) + " caras" ##esta y las siguientes 5 lineas dan las condiciones del texto que da el numero de caras""
 ubicacion = (200,700)
 font = cv2.FONT_HERSHEY_TRIPLEX
 tamañoLetra = 5
 colorLetra = (221,82,196)
 grosorLetra = 10
 
-#Escribir texto
+#Escribir texto en la imágen
 cv2.putText(img, texto, ubicacion, font, tamañoLetra, colorLetra, grosorLetra)
-cv2.imshow(winname="Face", mat=img)
+cv2.imshow(winname="Face", mat=img)  ##muestra la imágen##
 
-cv2.waitKey(delay=0)
+cv2.waitKey(delay=0) ##cierra la imágen al tocar una tecla##
 
-cv2.destroyAllWindows()
-print("hola")
+cv2.destroyAllWindows()##hace que se cierre la ventana##
+
 
